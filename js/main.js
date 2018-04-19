3ca2a0d3c8a3718bb2ddb1aece10ac30ad28ba51
294780130 <294780130@qq.com>
2018-04-19 22:52:39 +0800

    22点52分
    first day

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// JavaScript Document
+$(function (){
+	 $('#carousel').mouseover(function(){
+		$('#left').removeClass('unable');
+		$('#right').removeClass('unable');
+	 });
+	$('#carousel').mouseout(function(){
+		$('#left').addClass('unable');
+		$('#right').addClass('unable');
+	 });
+});
